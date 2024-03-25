eabb21706c999e4b2a98decb299a32928160403f
Marcos Rodríguez <e.marcosra@gmail.com>
2024-03-25 19:37:33 +0100

    Add task 1

--- src/giveaways.ts
+++ src/giveaways.ts
@@ -3,3 +3,17 @@ import { programData, saveData } from "./storage.js";
 
 import { Giveaway } from "./types.js";
 import { askUserNewGiveawayData } from "./ui.js";
+
+export const loginUser = (email: string, password: string): void => {
+  const user = programData.users.find(
+    (user) => user.email === email && user.password === password
+  );
+
+  if (user?.email === email && user.password === password) {
+    programData.userEmail = email;
+    programData.isAdmin = user.isAdmin;
+  } else {
+    console.log("Error, el usuario no existe");
+    process.exit();
+  }
+};
